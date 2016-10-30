eacd3c27c743aa530193c2ea3ed95d236ea47659
hicwic <steve.eckerlein@gmail.com>
2016-10-30 10:14:54 +0100

    Splitting extraction in two part extract/rendering

--- src/face_detection_package/face_detection.py
+++ src/face_detection_package/face_detection.py
@@ -12,7 +12,7 @@ class landmarks_extractor:
 	def __init__(self):
 		#Set up some required objects
 		self.detector = dlib.get_frontal_face_detector() #Face detector
-		self.predictor = dlib.shape_predictor("/home/zoid/catkin_ws/src/extract_face_feature/dat/shape_predictor_68_face_landmarks.dat") #Landmark identifier.
+		self.predictor = dlib.shape_predictor("/home/zoid/catkin_ws/src/face_features/dat/shape_predictor_68_face_landmarks.dat") #Landmark identifier.
 
 
 	def prepareImage (self, frame):
@@ -23,18 +23,12 @@ class landmarks_extractor:
 
 
 	def getLandmarksImage (self):
-		for index in range(len(self.faces)):
-			landmarksImage = self.getLandmarksImageForFace(index)
-			
-		return landmarksImage
-
-
-	def getLandmarksImageForFace (self, faceId):
 		landmarksImage = self.frame
-
-		for landmark in self.faces[faceId].landmarks:
-			cv2.circle(landmarksImage, (landmark.x, landmark.y), 1, (0,0,255), thickness=2) 
-
+	
+		for index in range(len(self.faces)):
+			for landmark in self.faces[index].landmarks:
+				cv2.circle(landmarksImage, (landmark.x, landmark.y), 1, (0,0,255), thickness=2) 
+			
 		return landmarksImage
 
 
@@ -48,8 +42,6 @@ class landmarks_extractor:
 	def getNormalizedLandmarksImageForFace (self, faceId):
 		face = self.faces[faceId]
 
-		face.setScaleTargetSize(256)	
-	
 		rospy.logdebug("Image Size %sx%s", int(256*face.horizontalRatio), int(256*face.verticalRatio))
 		#crop image [Y1:Y2, X1:X2] 
 		landmarksImage = cv2.resize(self.frame[face.minPoint.y:face.maxPoint.y, face.minPoint.x:face.maxPoint.x], (int(256*face.horizontalRatio), int(256*face.verticalRatio))) 
@@ -59,15 +51,21 @@ class landmarks_extractor:
 
 		return landmarksImage
 		
+		
+	def getFaces (self):
+		return self.faces
 
 
 	def extract (self):
 		detections = self.detector(self.preparedFrame, 1) #Detect the faces in the image
 
+		rospy.logdebug("bla")
 		count = 0
 		self.faces = []
 		for k,d in enumerate(detections): #For each detected face
-			self.faces.append(landmarks_face(self.predictor(self.preparedFrame, d), count))
+			rospy.logdebug("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(k, d.left(), d.top(), d.right(), d.bottom()))
+			#rospy.logdebug("parts : {}".format(self.predictor(self.preparedFrame, d).parts[0]))
+			self.faces.append(landmarks_face(self.predictor(self.preparedFrame, d), count, 255))
 			count += 1
 		
 		return self.faces
@@ -76,7 +74,8 @@ class landmarks_extractor:
 
 class landmarks_face:
 
-	def __init__(self, shape, id):
+	def __init__(self, shape, id, targetSize):
+		self.id = id
 	
 		self.minPoint = point(-1, -1)
 		self.maxPoint = point(-1, -1)
@@ -100,13 +99,16 @@ class landmarks_face:
 				self.maxPoint.x = self.landmarks[i-1].x
 		
 			if self.maxPoint.y == -1 or self.maxPoint.y < shape.part(i).y:
-				self.maxPoint.y = self.landmarks[i-1].y			
+				self.maxPoint.y = self.landmarks[i-1].y		
+				
+		self.__setScaleTargetSize(targetSize)	
+
 			
 	def getLandmarks (self):
 		return self.landmarks
 			
 			
-	def setScaleTargetSize(self, targetSize):
+	def __setScaleTargetSize(self, targetSize):
 		self.scaleTargetSize = targetSize
 		
 		self.verticalRatio = 1
@@ -122,11 +124,12 @@ class landmarks_face:
 		scaleFactorX = targetSize/width*self.horizontalRatio
 		scaleFactorY = targetSize/height*self.verticalRatio
 
+		rospy.logdebug("Bouding box %s/%s - %s/%s", self.minPoint.x, self.minPoint.y, self.maxPoint.x, self.maxPoint.y)
 		rospy.logdebug("Width/Height %s/%s", width, height)
 		rospy.logdebug("Ratio H/V %s/%s", self.horizontalRatio, self.verticalRatio)		
 		rospy.logdebug("Scale ratio X/Y %s/%s", scaleFactorX, scaleFactorY)
 
-		self.setScaleFactor(scaleFactorX, scaleFactorY)
+		self.__setScaleFactor(scaleFactorX, scaleFactorY)
 
 		
 	def getVerticalRatio (self):
@@ -137,7 +140,7 @@ class landmarks_face:
 		return self.horizontalRatio		
 		
 
-	def setScaleFactor(self, scaleFactorX, scaleFactorY):
+	def __setScaleFactor(self, scaleFactorX, scaleFactorY):
 
 		self.scaledLandmarks = []
 		for landmark in self.landmarks:
@@ -146,7 +149,9 @@ class landmarks_face:
 			
 	def getScaledLandmarks (self):
 		return self.scaledLandmarks
-					
+			
+	def getid (self):
+		return self.id		
 			
 			
 			
